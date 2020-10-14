ae8037535d499529d82d2552ddd83cc7e6358618
Omkar <Omkar@192.168.0.15>
2020-10-14 12:12:26 -0400

    added updateUser() made changes in removeUser() and getUser()

--- hiberRegistration/src/main/java/com/example/hiberRegistration/controller/UserController.java
+++ hiberRegistration/src/main/java/com/example/hiberRegistration/controller/UserController.java
@@ -3,10 +3,12 @@ package com.example.hiberRegistration.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,23 +23,63 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
-	@GetMapping("/all")
-	public List<User> getAllUser() {
-		return userService.getAllUser();
-	}
-
-	@GetMapping("/{id}")
-	public User getUser(@PathVariable("id") Integer id) {
-		return userService.getUser(id);
-	}
-
 	@PostMapping("/add")
 	public String addUser(@RequestBody User user) {
 		return userService.addUser(user);
 	}
 
+	@GetMapping("/all")
+	public List<User> getAllUser() {
+		return userService.getAllUser();
+	}
+
+	/*
+	 * @GetMapping("/{id}") public User getUser(@PathVariable("id") Integer id) {
+	 * return userService.getUser(id); }
+	 */
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getUser(@PathVariable("id") Integer id) {
+		User user = userService.getUser(id);
+
+		if (user == null) {
+			return ResponseEntity.notFound().build();
+		}
+
+		return ResponseEntity.ok().body(user);
+	}
+
+	@PutMapping("/update/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable("id") Integer id, @RequestBody User userDetails) {
+		User user = userService.getUser(id);
+
+		if (user == null) {
+			return ResponseEntity.notFound().build();
+		}
+
+		if (userDetails.getName() != null)
+			user.setName(userDetails.getName());
+		if (userDetails.getEmail() != null)
+			user.setEmail(userDetails.getEmail());
+
+		userService.addUser(user);
+
+		return ResponseEntity.ok().body(user);
+
+	}
+
+	/*
+	 * @DeleteMapping("/remove/{id}") public String removeUser(@PathVariable("id")
+	 * Integer id) { return userService.removeUser(id); }
+	 */
+
 	@DeleteMapping("/remove/{id}")
 	public String removeUser(@PathVariable("id") Integer id) {
-		return userService.removeUser(id);
+		User user = userService.getUser(id);
+
+		if (user == null) {
+			return "User not found";
+		}
+		userService.removeUser(user);
+		return "User deleted from database";
 	}
 }
